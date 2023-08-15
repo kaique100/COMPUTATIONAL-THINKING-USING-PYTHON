f5053c937dfb653dfbd0faf2035962316ca9d76b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-14 22:58:53 -0300

    aula 14-8

---
+++ aula14-8.py
@@ -0,0 +1,44 @@
+
+
+# disct = {0:0,1:1,2:4,3:9}
+
+# dict={n:n**2}
+
+# def n_quadrado(N=1):
+    # dicio = {}
+    # for n in range(N+1):
+    #     dicio[n] = n**2
+
+#     dicio = {n:n**2 for n in range(N+1)}
+#     return dicio
+#
+# dicio_ex_1 = n_quadrado(5)
+#
+# print(dicio_ex_1)
+#
+#
+# def recebe_dicio_soma(dicionario):
+#    resposta = 0
+#    validador = True
+#    for valor in dicionario.values():
+#       if type(valor) == int or type(valor) == float:
+#         resposta += valor
+#         validador = False
+#
+#    if validador:
+#        return "Erro você não colocou um dicionario como parâmetro na chamada da função"
+#    else:
+#        return resposta
+#
+# dicio_ex_1 = n_quadrado()
+# print((dicio_ex_1))
+
+# lista1 = ["cor","tampa"]
+# lista2 = ["preta","branca"]
+# dicio = {}
+# for i in range(len(lista1)):
+#     dicio[lista1[i]] = lista2[i]
+#
+# print(dicio)
+
+
